5a0b408d556b7e5100e805b2ebd69b9aa42b2365
Erik Bourget <ebourget@gmail.com>
2012-07-05 14:24:24 -0700

    only take action if there is a difference

--- lib/fcm-agent-tools.rb
+++ lib/fcm-agent-tools.rb
@@ -1,5 +1,4 @@
 class FcmAgent
-  require 'tempfile'
   def initialize()
     unless ARGV.length == 1
       raise "This program takes exactly one argument"
@@ -17,12 +16,28 @@ class FcmAgent
     end
   end
 
+  # true: there is a difference.  false: there is not
+  def diff(file1, data)
+    return true unless File.exists?(file1)
+    f1 = File.open(file1)
+    d1 = f1.read
+    f1.close
+    return ! (d1 == data)
+  end
+
+  # true: something was changed.  false: it was not
   def install_file(location)
+    return false unless diff(location, @filedata)
+
+    STDERR.puts("#{$0}: Installing #{@filename} to #{location}")
+    require 'tempfile'
     filedir = File.dirname(@filename)
     newfile = Tempfile.new(filedir)
     newfile.write(@filedata)
     File.rename(newfile.path, location)
     newfile.fsync
     newfile.close
+
+    return true
   end
 end
