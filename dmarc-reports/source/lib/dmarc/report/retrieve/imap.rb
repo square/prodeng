2c29404ab92a2052a13b74c06bf1e5456385cfd8
Syam Puranam <github+sp@squareup.com>
2013-07-08 20:04:00 -0700

    stuff to retrieve from imap

--- dmarc-reports/source/lib/dmarc/report/retrieve/imap.rb
+++ dmarc-reports/source/lib/dmarc/report/retrieve/imap.rb
@@ -1,4 +1,7 @@
 require 'net/imap'
+require 'mail'
+require 'zipruby'
+
 module DMARC
   class Report
     class Retrieve
@@ -9,23 +12,44 @@ module DMARC
 end
 
 class DMARC::Report::Retrieve::IMAP
-  def initialize(server,username,password,folder)
-    folder ||= 'INBOX'
-    imap = Net::IMAP.new(server, 993, true)
+  def initialize(args)
+    folder = args[:folder] || 'INBOX'
+    server = args[:server] || 'server'
+    port   = args[:port]   ||  993
+    ssl    = args[:ssl]    ||  false
+    imap = Net::IMAP.new(server, port, ssl)
     imap.login(user, password)
     imap.select(folder)
   end
 
-  def fetch(mark_seen)
-    imap.search(["NOT", "SEEN"]).each do |message_id|
+  def fetch_aggregate_reports(mark_seen)
+    result = []
+    imap.search(["UNSEEN"]).each do |message_id|
+      body = imap.fetch(message_id, "BODY[]")[0].attr["BODY[]"]
+      mail = Mail.new(body)
+      mail.attachments.each do |a|
+        Zip::Archive.open_buffer(a.body.decoded) do |zf|
+          zf.fopen(zf.get_name(0)) do |f|
+            m = DMARC::Report::Aggregate.new
+            m.parse(f.read)
+            if(block_given?)
+              yield m
+            else
+              result.push(m)
+            end
+          end
+        end
+      end
+      if(mark_seen)
+        imap.store(message_id, "+FLAGS", [:Seen])
+      end
     end
-    if(mark_seen)
-      imap.store(message_id, "+FLAGS", [:Seen])
-    end
+    return result
   end
 
   def finalize()
     imap.logout()
     imap.disconnect()
   end
+
 end
