e56bca676936f3422c7d6a155c9767d907c1f3df
Evan Miller <evan@squareup.com>
2013-08-01 07:50:04 -0700

    travis-ci

---
+++ doit.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# ugly hack to build/test multiple projects in one repo
+
+set -x
+set -e
+
+for i in */; do
+  if [ -e "${i}doit.sh" ]; then
+    echo "BUILDING PROJECT $i"
+    pushd .
+    cd "$i"
+    "./doit.sh"
+    popd
+  fi
+done
