cfabb9cee652ee6b94a85feb05cd04b704659f24
Syam Puranam <github+sp@squareup.com>
2014-05-04 18:29:15 -0700

    Per PID statistics for macosx

---
+++ inspect/pidstat/pidstat_darwin.go
@@ -0,0 +1,264 @@
+// Copyright (c) 2014 Square, Inc
+
+package pidstat
+
+import (
+	"math"
+	_ "os/user"
+	"fmt"
+	"sort"
+	"time"
+	"unsafe"
+	"reflect"
+	"github.com/square/prodeng/inspect/misc"
+	"github.com/square/prodeng/metrics"
+)
+
+
+/*
+#include <mach/mach.h>
+#include <mach/task_info.h>
+#include <sys/sysctl.h>
+*/
+import "C"
+
+type ProcessStat struct {
+	Processes  map[string]*PerProcessStat
+	m          *metrics.MetricContext
+	hport	   C.host_t
+
+}
+
+// NewProcessStat allocates a new ProcessStat object
+// Arguments:
+// m - *metricContext
+
+// Collects metrics every m.Step seconds
+// Drops refresh interval by m.Step for every additional
+// 1024 processes
+// TODO: Implement better heuristics to manage load
+//   * Collect metrics for newer processes at faster rate
+//   * Slower rate for processes with neglible rate?
+
+func NewProcessStat(m *metrics.MetricContext) *ProcessStat {
+	c := new(ProcessStat)
+	c.m = m
+
+	c.Processes = make(map[string]*PerProcessStat, 1024)
+	c.hport = C.host_t(C.mach_host_self())
+
+	var n int
+	ticker := time.NewTicker(m.Step)
+	go func() {
+		for _ = range ticker.C {
+			p := int(len(c.Processes) / 1024)
+			if n == 0 {
+				c.Collect(true)
+			}
+			// always collect all metrics for first two samples
+			// and if number of processes < 1024
+			if p < 1 || n%p == 0 {
+				c.Collect(false)
+			}
+			n++
+		}
+	}()
+
+	return c
+}
+
+// ByCPUUsage implements sort.Interface for []*PerProcessStat based on
+// the Usage() method
+
+type ByCPUUsage []*PerProcessStat
+
+func (a ByCPUUsage) Len() int           { return len(a) }
+func (a ByCPUUsage) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByCPUUsage) Less(i, j int) bool { return a[i].CPUUsage() > a[j].CPUUsage() }
+
+// ByCPUUsage() returns an slice of *PerProcessStat entries sorted
+// by CPU usage
+func (c *ProcessStat) ByCPUUsage() []*PerProcessStat {
+	v := make([]*PerProcessStat, 0)
+	for _, o := range c.Processes {
+		if !math.IsNaN(o.CPUUsage()) {
+			v = append(v, o)
+		}
+	}
+	sort.Sort(ByCPUUsage(v))
+	return v
+}
+
+type ByMemUsage []*PerProcessStat
+
+func (a ByMemUsage) Len() int           { return len(a) }
+func (a ByMemUsage) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByMemUsage) Less(i, j int) bool { return a[i].MemUsage() > a[j].MemUsage() }
+
+// ByMemUsage() returns an slice of *PerProcessStat entries sorted
+// by Memory usage
+func (c *ProcessStat) ByMemUsage() []*PerProcessStat {
+	v := make([]*PerProcessStat, 0)
+	for _, o := range c.Processes {
+		if !math.IsNaN(o.MemUsage()) {
+			v = append(v, o)
+		}
+	}
+	sort.Sort(ByMemUsage(v))
+	return v
+}
+
+// reference /usr/include/mach/task_info.h
+// works on MacOSX 10.9.2; YMMV might vary
+
+func (c *ProcessStat) Collect(collectAttributes bool) {
+
+	fmt.Println(len(c.Processes))
+	h := c.Processes
+	for _, v := range h {
+		v.dead = true
+	}
+
+	var pDefaultSet C.processor_set_name_t
+	var pDefaultSetControl C.processor_set_t
+	var tasks C.task_array_t
+	var taskCount C.mach_msg_type_number_t
+
+	if C.processor_set_default(c.hport,&pDefaultSet) != C.KERN_SUCCESS {
+		return
+	}
+
+	// get privileged port to get information about all tasks
+
+	if C.host_processor_set_priv(C.host_priv_t(c.hport),
+		pDefaultSet,&pDefaultSetControl) != C.KERN_SUCCESS {
+		return
+	}
+
+
+	if C.processor_set_tasks(pDefaultSetControl, &tasks, &taskCount) != C.KERN_SUCCESS {
+		return
+	}
+
+	// convert tasks to a Go slice
+	hdr := reflect.SliceHeader{
+		Data: uintptr(unsafe.Pointer(tasks)),
+		Len: int(taskCount),
+		Cap: int(taskCount),
+	}
+
+	goTaskList := *(*[]C.task_name_t)(unsafe.Pointer(&hdr))
+
+	// mach_msg_type_number_t - type natural_t = uint32_t
+	var i uint32
+	now := time.Now().UnixNano()
+	for i = 0; i < uint32(taskCount); i++ {
+
+		taskId := goTaskList[i]
+		var pid C.int
+		// var tinfo C.task_info_data_t
+		var taskBasicInfoCount C.mach_msg_type_number_t
+		var taskInfo C.mach_task_basic_info_data_t
+
+		if (C.pid_for_task(C.mach_port_name_t(taskId),&pid) != C.KERN_SUCCESS) ||
+		   (pid < 0) {
+			continue
+		}
+
+		taskBasicInfoCount = C.MACH_TASK_BASIC_INFO_COUNT
+		kr := C.task_info(taskId, C.MACH_TASK_BASIC_INFO ,
+				  (C.task_info_t)(unsafe.Pointer(&taskInfo)),
+				  &taskBasicInfoCount)
+		if kr != C.KERN_SUCCESS {
+			continue
+		}
+
+		spid := fmt.Sprintf("%v",pid)
+		pidstat, ok := h[spid]
+		if !ok {
+			pidstat = NewPerProcessStat(c.m,spid)
+			h[spid] = pidstat
+		}
+
+		if collectAttributes || !ok {
+			pidstat.CollectAttributes()
+		}
+
+		pidstat.Metrics.VirtualSize.Set(float64(taskInfo.virtual_size))
+		pidstat.Metrics.ResidentSize.Set(float64(taskInfo.resident_size))
+		pidstat.Metrics.ResidentSizeMax.Set(float64(taskInfo.resident_size_max))
+		pidstat.Metrics.UserTime.Set(nanoSeconds(taskInfo.user_time))
+		pidstat.Metrics.SystemTime.Set(nanoSeconds(taskInfo.system_time))
+		pidstat.Metrics.UpdatedAt.Set(uint64(now - pidstat.Metrics.StartedAt))
+		pidstat.dead = false
+	}
+
+	// remove dead processes
+	for k, v := range h {
+		if v.dead {
+			delete(h, k)
+		}
+	}
+
+}
+
+func nanoSeconds(t C.time_value_t) uint64 {
+	a := t.seconds * (1000 * 1000) // nanoseconds
+	b := t.microseconds
+
+	return uint64(a+b) // we dont care about overflow
+}
+
+
+// Per Process functions
+type PerProcessStat struct {
+	Pid               string
+	Uid               int
+	User              string
+	Comm              string
+	Metrics  *PerProcessStatMetrics
+	m        *metrics.MetricContext
+	dead	  bool
+}
+
+func NewPerProcessStat(m *metrics.MetricContext, p string) *PerProcessStat {
+	c := new(PerProcessStat)
+	c.m = m
+	c.Pid = p
+	c.Metrics = NewPerProcessStatMetrics(m)
+	return c
+}
+
+func (s *PerProcessStat) CPUUsage() float64 {
+	o := s.Metrics
+	t := (o.UpdatedAt.CurRate() / float64(time.Second.Nanoseconds()))
+	return (o.UserTime.CurRate() + o.SystemTime.CurRate()) / t
+}
+
+func (s *PerProcessStat) MemUsage() float64 {
+	o := s.Metrics
+	return o.ResidentSize.Get()
+}
+
+type PerProcessStatMetrics struct {
+	VirtualSize       *metrics.Gauge
+	ResidentSize      *metrics.Gauge
+	ResidentSizeMax   *metrics.Gauge
+	UserTime          *metrics.Counter
+	SystemTime        *metrics.Counter
+	UpdatedAt         *metrics.Counter
+	StartedAt	  int64
+}
+
+func NewPerProcessStatMetrics(m *metrics.MetricContext) *PerProcessStatMetrics {
+	s := new(PerProcessStatMetrics)
+	s.StartedAt = time.Now().UnixNano()
+	// initialize all metrics
+	misc.InitializeMetrics(s,m)
+	return s
+}
+
+
+func (s *PerProcessStat) CollectAttributes() {
+
+}
